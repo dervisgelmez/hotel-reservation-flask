ff77d0e9fba37090b314be8aeb1d63558a77b7d4
dervis <dervis.gelmez@path.com.tr>
2019-12-02 18:17:31 +0300

    added 404 pages

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("gallery.html")
 
+@app.errorhandler(404)
+def page_not_found(error):
+   return render_template('404.html')
+
 if __name__ == "__main__":
     app.run(debug = True)
